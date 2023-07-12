d35aeec81004e333fbf02f31d2df46de06ba5384
sblekic <blekic.sandi@gmail.com>
2023-07-12 17:16:44 +0200

    ciscenje zbog test deploy na netlify

--- src/router/index.js
+++ src/router/index.js
@@ -12,14 +12,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/create-event",
-      name: "CreateEvent",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
       path: "/:eventId",
       name: "EventDetails",
       component: EventView,

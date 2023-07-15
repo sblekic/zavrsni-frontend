d34e916e99e9c5898abcea2fb744f200e6431bb9
sblekic <blekic.sandi@gmail.com>
2023-07-15 17:08:02 +0200

    fixed buffer is not defined while importing siwe

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,47 @@
 <script setup>
 import { MetaMaskSDK } from "@metamask/sdk";
+import { BrowserProvider } from "ethers";
+import { SiweMessage } from "siwe";
+
 const MMSDK = new MetaMaskSDK();
+const provider = new BrowserProvider(window.ethereum);
+
+const domain = window.location.host;
+const origin = window.location.origin;
+
+function createSiweMessage(address, statement) {
+  const message = new SiweMessage({
+    domain,
+    address,
+    statement,
+    uri: origin,
+    version: "1",
+    chainId: 1,
+  });
+  return message.prepareMessage();
+}
+
+async function connectWallet() {
+  const ethereum = MMSDK.getProvider(); // isto što i window.ethereum
+  try {
+    ethereum.request({ method: "eth_requestAccounts", params: [] });
+  } catch (error) {
+    // user rejected the request - code 4001; uncaught promise. Probao sam sve moguće kombinacije ali catch ne uhvati error kada user izađe iz connection screen.
+    // mislim da je razlog to što MM sdk renderira modal napisan u react-u preko kojeg se započinje request za spajanje walleta, i valjda u taj modal nema error handling?
+    // mogao bih se spojiti na običan način ali onda gubim funkcionalnost na mobile
+    console.error(error);
+  }
+}
 
 async function test() {
-  const ethereum = MMSDK.getProvider();
-  ethereum.request({ method: "eth_requestAccounts", params: [] });
-  // let metamask = await window.ethereum.request({
-  //   method: "eth_requestAccounts",
-  // });
-  // log.metamask;
-  console.log("sup");
+  try {
+    const signer = await provider.getSigner();
+    // const message = createSiweMessage(
+    //   signer.address,
+    //   "Sign in with Ethereum to the app."
+    // );
+    // console.log(await signer.signMessage(message));
+  } catch (error) {}
 }
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 // dodani aliases for quality of life
 import path from "path";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
